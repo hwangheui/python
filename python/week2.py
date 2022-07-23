b879749995ef17682ff0922c0300c0e75d8defe0
hwangheui <superegohwang@gmail.com>
2022-07-23 18:30:18 +0900

    first commit

---
+++ python/week2.py
@@ -0,0 +1,39 @@
+# week2
+"""
+안녕하세요 저는 황희입니다
+"""
+"""
+#사칙연산 츌력
+print(1+2) #덧셈
+print(1-3) #뺄셈
+print(3*4) #곱셈
+print(4/3) #나눗셈 (몫과 나머지)
+       
+print(2**10) #제곱근
+print(90//4) #몫
+print(5%4) #나머지
+"""
+# 자기소개
+# print("안녕하세요")
+# print('저는 황희입니다')
+# print('저는 잠을 자는 것을 좋아합니다')
+# print("저는 과천중앙고에 다닙니다")
+# print('저는 17살입니다')
+
+###  (자료형)
+## type( ) : 어떠한 값의 자료형을 반환해주는 함수
+#print(type(9)) #int : 정수 
+# print(type(9.9999)) #float : 실수
+# print(type("ㄱ")) #str : 문자열(string)
+
+
+print(type(1+2)) #덧셈
+print(type(1-3))#뺄셈
+print(type(3*4)) #곱셈
+print(type(4/3)) #나눗셈 (몫과 나머지)
+print(type(10%2)) #나머지
+
+
+
+
+
